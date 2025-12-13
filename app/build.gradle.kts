98eb1b6bcc7bef537505fb7ee302b6736df33ee8
thiago082882 <thiagocostadejesus2@gmail.com>
2025-12-13 15:30:38 -0300

    ci:criando as tasks personalizada

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,11 +46,6 @@ android {
         compose = true
     }
 
-    bundle {
-        language { enableSplit = false }
-        density  { enableSplit = false }
-        abi      { enableSplit = false }
-    }
 }
 
 dependencies {
